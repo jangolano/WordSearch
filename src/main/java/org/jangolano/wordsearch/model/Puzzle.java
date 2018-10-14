1aed51496ca458fd4b245db1dcacbfa1ecf996cf
Josh Angolano <grangolano@gmail.com>
2018-10-14 14:28:52 -0500

    Initial Commit - Failing to get the value at a point

---
+++ src/main/java/org/jangolano/wordsearch/model/Puzzle.java
@@ -0,0 +1,14 @@
+package org.jangolano.wordsearch.model;
+
+public class Puzzle {
+
+    char [][] data;
+    Puzzle(char [][] data){
+        this.data = data;
+    }
+
+    char getValueAt(int row, int column){
+        return 'z';
+    }
+
+}
---
+++ src/test/java/org/jangolano/wordsearch/model/PuzzleTest.java
@@ -0,0 +1,19 @@
+package org.jangolano.wordsearch.model;
+
+import org.junit.Test;
+
+public class PuzzleTest {
+
+    @Test
+    public void getValueAt(){
+        char [][] data = {
+            { 'A', 'B', 'C', 'D'},
+            { 'E', 'F', 'G', 'H'},
+            { 'I', 'J', 'K', 'L'},
+            { 'M', 'N', 'O', 'P'}
+        };
+        Puzzle puzzle = new Puzzle(data);
+        char value  = puzzle.getValueAt(3, 2);
+        assert(value =='O');
+    }
+}
