644934307ae426729a80192fef4d11e51df89c62
Josh Angolano <grangolano@gmail.com>
2018-10-14 18:27:13 -0500

    Refactored to be easier to read and removed redudent variables

--- src/main/java/org/jangolano/wordsearch/model/Puzzle.java
+++ src/main/java/org/jangolano/wordsearch/model/Puzzle.java
@@ -6,7 +6,7 @@ import java.util.function.Function;
 public class Puzzle {
 
     char [][] data;
-    final char NULL_VALUE = '\u0000';
+    final private char NULL_VALUE = '\u0000';
 
     Puzzle(char [][] data){
         this.data = data;
@@ -32,7 +32,7 @@ public class Puzzle {
 
     //Method to get locations of a value in the array.
     ArrayList<Point> getLocationsOf(char value){
-        ArrayList<Point> instances = new ArrayList<Point>();
+        ArrayList<Point> instances = new ArrayList<>();
         for (int row = 0; row<data.length; row++){
 
             for(int col = 0; col<data[row].length ;col++){
@@ -44,6 +44,37 @@ public class Puzzle {
         }
         return instances;
     }
+
+
+    //Method to get the all the locations of the first character of a string.
+    private ArrayList<Point> getInitalLocations(char aChar) {
+        return getLocationsOf(aChar);
+    }
+
+    //Search for values within the puzzle
+    private ArrayList<Point> findValues(String word, Function<Point, Point > f){
+        char [] chars = word.toCharArray();
+        ArrayList<Point> locations = getInitalLocations(chars[0]);
+
+        for(Point point:locations){
+            ArrayList<Point> results = new ArrayList<>();
+            results.add(point);
+            Point nextPoint = point ;
+            for(int index = 1; index<chars.length; index++){
+                nextPoint = f.apply(nextPoint);
+                if(getValueAt(nextPoint)==chars[index]){
+                    results.add(nextPoint);
+                }
+            }
+
+            //If all the letters are found return
+            if(results.size()==chars.length){
+                return results;
+            }
+        }
+        return new ArrayList<>();
+    }
+
 
     //Find a character and then search to the right for the rest of the values with the word.
     ArrayList<Point> searchHorizontal(String word){
@@ -70,35 +101,5 @@ public class Puzzle {
         return findValues(word, Point::getTopRight);
     }
 
-    //Search for values within the puzzle
-    private ArrayList<Point> findValues(String word, Function<Point, Point > f){
-        char [] chars = word.toCharArray();
-        ArrayList<Point> locations = getInitalLocations(chars[0]);
-
-        for(Point point:locations){
-            ArrayList<Point> results = new ArrayList<Point>();
-            results.add(point);
-            Point nextPoint = point ;
-            for(int index = 1; index<chars.length; index++){
-                nextPoint = f.apply(nextPoint);
-                if(getValueAt(nextPoint)==chars[index]){
-                    results.add(nextPoint);
-                }
-            }
-
-            //If all the letters are found return
-            if(results.size()==chars.length){
-                return results;
-            }
-        }
-        return new ArrayList<Point>();
-    }
-
-
-    //Method to get the all the locations of the first character of a string.
-    private ArrayList<Point> getInitalLocations(char aChar) {
-        char character = aChar; //Get the first character in the word and find its locations;
-        return getLocationsOf(character);
-    }
 
 }
