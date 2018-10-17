d96aa97f1ce7489bff9ddf370a3bab1f82fff7a9
Josh Angolano <grangolano@gmail.com>
2018-10-16 22:22:10 -0500

    Added comment to Result Printer Class

--- src/main/java/org/jangolano/wordsearch/ResultPrinter.java
+++ src/main/java/org/jangolano/wordsearch/ResultPrinter.java
@@ -1,11 +1,29 @@
+/**
+ * This class is used to print the results of the program to the screen.
+ */
 package org.jangolano.wordsearch;
+
+import java.util.ArrayList;
 
 public class ResultPrinter {
 
     public  ResultPrinter(){
 
     }
+
+    //Get the string results
     public String getResults(String[] words, Puzzle puzzle) {
-        return "";
+        StringBuffer results = new StringBuffer("");
+        for (String word : words) {
+            ArrayList<Point> points = puzzle.searchPuzzle(word);
+            results.append(word + ": ");
+            for (Point point : points) {
+                results.append("(" + point.getColumn() + "," + point.getRow() + "),");
+            }
+            results.deleteCharAt(results.lastIndexOf(","));
+            results.append("\n");
+        }
+        results.deleteCharAt(results.lastIndexOf("\n"));
+        return results.toString();
     }
 }
